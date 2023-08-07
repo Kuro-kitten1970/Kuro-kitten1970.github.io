e85747fa770423eaa641c1379aa2cfc22e410443
Kuro_kitten <slothguylazy330@gmail.com>
2023-08-07 17:13:01 +0700

    Optimize HTML

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const newHeader = document.createElement("header");
+const navhtml =
+    '<h2><a href="index.html">Kuro_kitten Backyard</a></h2>'
+    + '<nav>'
+    + '<div><a href="index.html">Home</a></div>'
+    + '<div><a href="work.html">Work</a></div>'
+    + '<div><a href="contact.html">Contact</a></div>'
+    + '</nav>';
+navhtml.trim();
+newHeader.innerHTML = navhtml;
+document.getElementById("nav").appendChild(newHeader);
+
+const newFooter = document.createElement("footer");
+const footerhtml = '<p>&copy; Copyright Kuro_kitten 2023</p>';
+footerhtml.trim();
+newFooter.innerHTML = footerhtml;
+document.getElementById("footer").appendChild(newFooter);
