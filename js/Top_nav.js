90c57af48ffed53f3f1141d38a16c1c4cce358db
Kuro_kitten <slothguylazy330@gmail.com>
2023-10-08 03:36:13 +0700

    Update Contact Page

--- js/Top_nav.js
+++ js/Top_nav.js
@@ -1,6 +1,6 @@
 const newHeader = document.createElement("header");
 const navhtml =
-    '<h2><a href="index.html">Kuro_kitten Backyard</a></h2>'
+    '<h2><a href="index.html">Kuro_kitten\'s Backyard</a></h2>'
     + '<nav>'
     + '<div><a href="index.html">Home</a></div>'
     + '<div><a href="work.html">Work</a></div>'
@@ -11,7 +11,7 @@ newHeader.innerHTML = navhtml;
 document.getElementById("nav").appendChild(newHeader);
 
 const newFooter = document.createElement("footer");
-const footerhtml = '<p>Kuro_kitten &copy;Copyright 2023</p>';
+const footerhtml = '<p>Kuro_kitten &copy; Copyright 2023</p>';
 footerhtml.trim();
 newFooter.innerHTML = footerhtml;
 document.getElementById("footer").appendChild(newFooter);
